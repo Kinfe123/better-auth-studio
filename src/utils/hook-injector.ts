c37bae794f151a0c972a2ae1d7c8fc15169bbcc0
Kinfe123 <kinfishtech@gmail.com>
2026-01-15 05:27:41 +0300

    chore: hooks injectors

--- src/utils/hook-injector.ts
+++ src/utils/hook-injector.ts
@@ -2,7 +2,7 @@ import { getSessionFromCtx } from 'better-auth/api';
 import { createAuthMiddleware } from 'better-auth/plugins';
 import type { StudioConfig } from '../types/handler.js';
 import { emitEvent } from './event-ingestion.js';
-import { wrapOrganizationPluginHooks } from './org-hooks-wrapper.js';
+import { wrapOrganizationPluginHooks } from './org-hooks-injector.js';
 
 const INJECTED_HOOKS_MARKER = '__better_auth_studio_events_injected__';
 
